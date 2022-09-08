ef79f8de8fa10930fe7a05171f25cacd0e6cea24
Paulo Henrique <ph.jesus@gmail.com>
2022-09-07 23:45:54 -0300

    criando header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
 
 function App() {
   return (
-    <>    <h1>Olá mundo!</h1>
+    <> 
+    <Header />
     <NotificationButton />
     </>
 
