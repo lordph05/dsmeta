397daed0e9a2277643c9a4eea0b1e3efc5e53949
Paulo Henrique <ph.jesus@gmail.com>
2022-09-08 00:23:39 -0300

    começando o main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import NotificationButton from "./components/NotificationButton"
 import Header from "./components/Header"
+import salesCard from "./components/SalesCard"
+
 
 function App() {
   return (
     <> 
     <Header />
-    <NotificationButton />
+    <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+
+        </div>
+        </section>
+        </main>
+   
     </>
 
   
---
+++ frontend/src/components/SalesCard/index.tsx
@@ -0,0 +1,10 @@
+
+function salesCard() {
+  return (
+    <h1> Sales Card</h1>
+  
+  )
+  
+}
+
+export default salesCard
